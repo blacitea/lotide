513fc4bdd1c647059aa2508a0c4e10941144edc7
Angel Tsang <blacitea@gmail.com>
2020-07-21 01:35:48 +0000

    customize assert function with emoji ctrl+cmd+space

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌: [${actual}] !== [${expected}]`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamps");
+assertEqual(1, 1);
+//Comparing identical strings
+assertEqual("How are you today", "How are you today");
+//Comparing non-identical strings
+assertEqual("I like ice-cream", "I like hotpot");
+//Comparing identical numbers
+assertEqual("101", "101");
+//Comparing non-identical numbers
+assertEqual("9", "15");
