ba7a61109350763ac429a76650a92ca0843740b2
Angel Tsang <blacitea@gmail.com>
2020-07-22 21:14:00 +0000

    completed

--- countOnly.js
+++ countOnly.js
@@ -9,5 +9,38 @@ const assertEqual = function(actual, expected) {
 // Code for countOnly
 
 const countOnly = function (allItems, itemsToCount) {
-  
-}
+  const result = {};
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+
+
+// Code for Testing 
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
