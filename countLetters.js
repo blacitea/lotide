1783b3ba57112d8c81c2b31f2e8d2d01198acf44
Angel Tsang <blacitea@gmail.com>
2020-07-22 21:35:06 +0000

    removed space and added toLowerCase()

--- countLetters.js
+++ countLetters.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 // Code for countLetter
 const countLetter = function(string) {
   let result = {};
-  let letters = string.split("");
+  let letters = string.split(" ").join("").toLowerCase().split("");
   for (let letter of letters) {
     if (result[letter]) {
       result[letter] += 1;
@@ -17,12 +17,15 @@ const countLetter = function(string) {
       result[letter] = 1;
     }
   }
+  console.log(result);
   return result;
 };
 
 
 
 // Code for testing
-let toTest = "LHL";
-assertEqual(countLetter(toTest)["L"], 2);
-assertEqual(countLetter(toTest)["H"], 1);
+let testString = "Hello WOrld A a AA ";
+let toTest = countLetter(testString);
+assertEqual(toTest["o"], 2);
+assertEqual(toTest["h"], 1);
+assertEqual(toTest["l"], 3);
