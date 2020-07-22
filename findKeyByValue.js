c1098e88298d3de04178fe08eb5fd7cf20201c32
Angel Tsang <blacitea@gmail.com>
2020-07-22 22:19:58 +0000

    created function with return var

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,9 @@
 // Main Code
+const findKeyByValue = function(checkObject, searchValue) {
+  let result;
 
+  return result;
+};
 
 
 
@@ -15,3 +19,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Code for test
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
