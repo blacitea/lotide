2b5d26a7f61ed5f62a17f5b0008196dfcc5a5e21
Angel Tsang <blacitea@gmail.com>
2020-07-22 01:49:21 +0000

    eslint

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -9,3 +9,11 @@ const eqArrays = (array1, array2) => {
   }
   return true;
 };
+
+const assertArraysEqual = (arr1, arr2, evaluator) => {
+  return evaluator(arr1, arr2) ? console.log(`✅ It's an exact match!`) : console.log(`❌ No they are not the same`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3], eqArrays);
+assertArraysEqual(["1", "2", 3], [1, "2", "3"], eqArrays);
+assertArraysEqual([1, "2", 3], [1, "2", 3], eqArrays);
