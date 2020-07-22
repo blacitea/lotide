a3afe8d33a3e4427904ca5056b6f9d516d332a31
Angel Tsang <blacitea@gmail.com>
2020-07-22 22:16:35 +0000

    create file with assertEqual

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+// Main Code
+
+
+
+
+
+
+
+// Code for assertion
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌: [${actual}] !== [${expected}]`);
+  }
+};
+
