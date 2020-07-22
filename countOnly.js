0e0ee7f557312fdcbd824eaf60128b4f14b28d98
Angel Tsang <blacitea@gmail.com>
2020-07-22 20:53:19 +0000

    declare empty function

---
+++ countOnly.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌: [${actual}] !== [${expected}]`);
+  }
+};
+
+// Code for countOnly
+
+const countOnly = function (allItems, itemsToCount) {
+  
+}
