a70383739b2a1fe383884c45c956b38a9263239f
Angel Tsang <blacitea@gmail.com>
2020-07-23 22:56:52 +0000

    completed

--- findKey.js
+++ findKey.js
@@ -5,3 +5,25 @@ const assertEqual = function(actual, expected) {
     console.log(`❌: [${actual}] !== [${expected}]`);
   }
 };
+
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+
+// Test code
+let results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2);
+
+console.log(results1);
+assertEqual(results1, "noma");
