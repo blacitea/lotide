078b0a96bd4a2258bd0b884168f474ebb6895443
Angel Tsang <blacitea@gmail.com>
2020-07-23 21:50:51 +0000

    added var result for return

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function () {
-  
+  const results = [];
+  return results;
+
 }
 
 const result1 = map(words, word => word[0]);
