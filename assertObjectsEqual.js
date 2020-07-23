eefed57968bf8a6662195a8265ee8f48d92962e2
Angel Tsang <blacitea@gmail.com>
2020-07-23 00:48:42 +0000

    eslint

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,5 @@
 // Main code
-const assertObjectsEqual = function (actual, expected) {
+const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   // console.log(`Example label: ${inspect(actual)}`);
   if (eqObjects(actual, expected)) {
