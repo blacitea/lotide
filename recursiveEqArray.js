921df06cbddc76944f14930490c8e2fdeeff2f98
Angel Tsang <blacitea@gmail.com>
2020-07-25 02:10:44 +0000

    added new test code

--- recursiveEqArray.js
+++ recursiveEqArray.js
@@ -30,14 +30,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[2, 3], [4, [1, 2, [3]]]], [[2, 3], [4, [1, 2, [3]]]]), true); // => false
 
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 // assertEqual(eqArrays(["1", "2", 3], ["1", "2", 3]), true);
 // assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false);
 // assertEqual(eqArrays([1, [2, 4], "3"], [1, [2, 4], "3"]), true);
-assertEqual(eqArrays([1, [2, 5], "3"], [1, [2, 4], "3"]), false);
+// assertEqual(eqArrays([1, [2, 5], "3"], [1, [2, 4], "3"]), false);
 
-assertEqual(eqArrays([1, [2, 4, [6]], "3"], [1, [2, 4, [1, 5]], "3"]), false);
+// assertEqual(eqArrays([1, [2, 4, [6]], "3"], [1, [2, 4, [1, 5]], "3"]), false);
 
-assertEqual(eqArrays([1, [2, 4,["1", 5]], "3"], [1, [2, 4, [1, 5]], "3"]), false);
+// assertEqual(eqArrays([1, [2, 4,["1", 5]], "3"], [1, [2, 4, [1, 5]], "3"]), false);
