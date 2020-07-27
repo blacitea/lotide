b9bcb1804aeace24370bbf78f160e5dc1406fa47
Angel Tsang <blacitea@gmail.com>
2020-07-27 20:35:47 +0000

    refactor eqArrays

--- eqArray.js
+++ eqArray.js
@@ -15,18 +15,3 @@ const eqArrays = (array1, array2) => {
 };
 
 module.exports = eqArrays;
-
-// // Assertion code/ Reference code
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`❌: [${actual}] !== [${expected}]`);
-//   }
-// };
-
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", 3], ["1", "2", 3]), true);
-// assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false);
