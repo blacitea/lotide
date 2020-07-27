a9b73867d268c3f55d0050e179040c741afd14cf
Angel Tsang <blacitea@gmail.com>
2020-07-27 21:48:22 +0000

    refactored  test/tailTest.js , used assert.deepEqual instead of eqArrays

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
-const eqArrays = require("../eqArray");
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
-assertEqual(eqArrays(tail([1, 2, 3, 4, 5]), [1]), false); // false
-assertEqual(eqArrays(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]), true); // true
+describe("#Tail", () => {
+  it("should return [2, 3, 4, 5] when given [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("should return ['LightHouse', 'Labs'] when given ['YAY', 'LightHouse', 'Labs']", () => {
+    assert.deepEqual(tail(['YAY', 'LightHouse', 'Labs']), ['LightHouse', 'Labs']);
+  });
+  it("should not change the input array length when given [1, 2, 3], ", () => {
+    let array = [1, 2, 3];
+    assert.deepEqual(tail(array), [2, 3]);
+    assert.strictEqual(array.length, 3);
+  });
+});
 
-assertEqual(eqArrays(tail([1, 2, "hi", 4, ","]), [2, 'hi', 4, ',']), true);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(eqArrays(tail(words), ["Lighthouse", "Labs"]), true);
-assertEqual(words.length, 3);
