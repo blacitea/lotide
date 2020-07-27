eba5d34ccf8cf4d587caeaaf2a184331b9008ff5
Angel Tsang <blacitea@gmail.com>
2020-07-27 16:17:38 +0000

    double check to make sure code works as intented

--- assertEqual.js
+++ assertEqual.js
@@ -11,9 +11,13 @@ assertEqual("Lighthouse Labs", "Bootcamps");
 assertEqual(1, 1);
 //Comparing identical strings
 assertEqual("How are you today", "How are you today");
+//Comparing same strings with different upper/lower case
+assertEqual("HOW ARE you today", "How are you today");
 //Comparing non-identical strings
 assertEqual("I like ice-cream", "I like hotpot");
 //Comparing identical numbers
-assertEqual("101", "101");
+assertEqual(101, 101);
 //Comparing non-identical numbers
-assertEqual("9", "15");
+assertEqual(9, 15);
+//Comparing numbers and number string
+assertEqual(9, "9");
